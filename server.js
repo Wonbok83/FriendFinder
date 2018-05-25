b333010b448eb00104c6a34f77d548526fb9208a
Wonbok Yang <won-83@hotmail.com>
2018-05-25 13:13:50 -0500

    updating

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser = require("body-parser");
 // Tells node that we are creating an "express" server
 var app = express();
 
-var PORT = 3333; 
+var PORT = process.env.PORT || 3333; 
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
